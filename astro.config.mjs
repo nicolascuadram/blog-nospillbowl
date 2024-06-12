c20d539b38d5af6042dbb692ec73dc211a8585c0
Nico <ncuadra19@alumnos.utalca.cl>
2024-06-12 14:54:00 -0400

    Deploy 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://NicolasC-Utal.github.io',
-  base: 'nospillbowl',
+  base: 'blog-nospillbowl',
   integrations: [tailwind()]
 });

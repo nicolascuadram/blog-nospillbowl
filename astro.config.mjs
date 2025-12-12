8628b43e93d23ad6cf6de6dd03640bcbfcf5c3a8
Nicolás_Cuadra <nikocuadram@gmail.com>
2025-12-11 23:36:55 -0300

    Upgrade Astro and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nicolascuadram.github.io',
   base: 'blog-nospillbowl',
-  integrations: [tailwind()]
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
